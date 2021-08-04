0269ac8af053bc0021ba2f24e1bd7114a89cfd65
jontexman <jpmdoliveira@gmail.com>
2021-08-04 02:50:02 -0300

    Criaçao de um footer basico, esperando mais info para estilizar/adicionar detalhes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import GlobalStyle from './styles/global'
 import {BrowserRouter as Router} from 'react-router-dom'
 import MyRoutes from './Routes/index'
-import {Navbar} from "./components/Navbar"
+import {Navbar} from './components/Navbar'
+import {Footer} from './components/Footer'
 function App() {
   return (
-      <Router>
+    <div className="approute">
         <GlobalStyle/>  
         <Navbar/>
+      <Router>
         <MyRoutes/> 
       </Router>   
+        <Footer/>
+    </div>    
   );
 }
 
