60307f2dae3d3837d6db8c355ec45db8668cdef3
Gabriel Xavier <gabrielx@pop-os.localdomain>
2021-08-08 16:10:50 -0300

    :lipstick: criando pagina de artistas

---
+++ src/Artistas/index.js
@@ -0,0 +1,26 @@
+import Card from '../components/Cards';
+import { Container, ContainerCards } from './styled';
+
+const ArtistsPage = () => {
+    return (
+        <Container>
+            <h1>Explore Artistas</h1>
+            <ContainerCards>
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+            </ContainerCards>
+        </Container>
+    );
+}
+
+export default ArtistsPage;
